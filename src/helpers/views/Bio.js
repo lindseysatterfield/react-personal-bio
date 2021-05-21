5ea85bfe1031cd3a6ffdcb9739a474afb12a5d43
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-05-20 20:51:01 -0500

    added bio

--- src/helpers/views/Bio.js
+++ src/helpers/views/Bio.js
@@ -7,20 +7,21 @@ import lindsey from '../../assets/lindsey.png';
 export default function Bio() {
   return (
     <div className="header-container">
-      <div className="name-header-container">
-        <div className="header">
-          <h1 className="my-4">LINDSEY SATTERFIELD</h1>
-          <h3>Software Developer</h3>
-          <div id="contact-me">
-            <a href="mailto:satterfield.lindsey@gmail.com"><img src={mail} className="contact-icons mail" alt="mail icon"/></a>
-            <a href="https://www.linkedin.com/in/lindseysatterfield/"><img src={linkedin1} className="contact-icons linkedin" alt="linkedin icon"/></a>
-            <a href="https://github.com/lindseysatterfield"><img src={github1} className="contact-icons github" alt="github icon"/></a>
+        <div className="name-header-container">
+          <div className="header">
+            <h1 className="my-4">LINDSEY SATTERFIELD</h1>
+            <h3>software developer</h3>
+            <div id="contact-me">
+              <a href="mailto:satterfield.lindsey@gmail.com"><img src={mail} className="contact-icons mail" alt="mail icon"/></a>
+              <a href="https://www.linkedin.com/in/lindseysatterfield/"><img src={linkedin1} className="contact-icons linkedin" alt="linkedin icon"/></a>
+              <a href="https://github.com/lindseysatterfield"><img src={github1} className="contact-icons github" alt="github icon"/></a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="profile-img-container">
-        <img src={lindsey} className="lindsey-img" alt="Lindsey Memoji"/>
-      </div>
+        <div className="profile-img-container">
+          <img src={lindsey} className="lindsey-img" alt="Lindsey Memoji"/>
+        </div>
+        <p>As a long time enthusiast of all things technology, I am currently loving what I do -- developing websites and applications. A Nashville native, I am still here and currently attending Nashville Software School. Formerly a worker in the video production world and graduate of the University of Tennessee, I decided it was time to follow my dreams and enter the technology world to allow myself more creativity. When I am not in front of a computer, I enjoy hiking, photography, traveling, and going to Preds games.</p>
     </div>
   );
 }
