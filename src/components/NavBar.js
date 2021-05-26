fff4393ca13f7e1c84fe8df87934f9f0578a8fa9
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-05-25 22:45:08 -0500

    code clean up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import { NavHashLink } from 'react-router-hash-link';
-// import { HashLink } from 'react-router-hash-link';
 import {
   Collapse,
   Navbar,
