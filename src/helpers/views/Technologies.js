f7129aac865ea6ddd76bf74a5be27b3b5963d0f2
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-06-01 14:23:04 -0500

    added npm icon

--- src/helpers/views/Technologies.js
+++ src/helpers/views/Technologies.js
@@ -5,30 +5,33 @@ export default function Technologies() {
     <div className="mb-5">
       <h2 className="text-center my-3" id="technologies">technologies</h2>
       <ul className="technologies-container">
-      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-react fa-3x technology-links"></i></li>
-      </a>
-      <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-js fa-3x"></i></li>
-      </a>
-      <a href="https://en.wikipedia.org/wiki/HTML5" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-html5 fa-3x"></i></li>
-      </a>
-      <a href="https://en.wikipedia.org/wiki/CSS" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-css3-alt fa-3x"></i></li>
-      </a>
-      <a href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-sass fa-3x"></i></li>
-      </a>
-      <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-bootstrap fa-3x"></i></li>
-      </a>
-      <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-git fa-3x"></i></li>
-      </a>
-      <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-        <li><i className="fab fa-github fa-3x"></i></li>
-      </a>
+        <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-react fa-3x technology-links"></i></li>
+        </a>
+        <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-js fa-3x"></i></li>
+        </a>
+        <a href="https://en.wikipedia.org/wiki/HTML5" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-html5 fa-3x"></i></li>
+        </a>
+        <a href="https://en.wikipedia.org/wiki/CSS" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-css3-alt fa-3x"></i></li>
+        </a>
+        <a href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-sass fa-3x"></i></li>
+        </a>
+        <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-bootstrap fa-3x"></i></li>
+        </a>
+        <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-git fa-3x"></i></li>
+        </a>
+        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-github fa-3x"></i></li>
+        </a>
+        <a href="https://www.npmjs.com/" target="_blank" rel="noopener noreferrer">
+          <li><i className="fab fa-npm fa-3x"></i></li>
+        </a>
       </ul>
     </div>
   );
